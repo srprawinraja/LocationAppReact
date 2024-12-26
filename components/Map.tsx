bfbcf3c3ec0528aa503ff43d3d91019bbd062115
srprawinraja <srprawinraja@gmail.com>
2024-12-27 02:02:19 +0530

    changes

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,6 +1,6 @@
 import {Text} from 'react-native';
 import Mapbox, {MapView} from '@rnmapbox/maps';
-const accessToken = 'sk.eyJ1Ijoic3JwcmF3aW5yYWphIiwiYSI6ImNtNTVxcnlocDJ2cWoyaXNmcDllaDN1Z2MifQ.n8f7sDBpFA0sDVToxzLl7w';
+const accessToken = '';
 Mapbox.setAccessToken(accessToken);
 export default function Map() {
     return <MapView style={{flex:1}}/>;
