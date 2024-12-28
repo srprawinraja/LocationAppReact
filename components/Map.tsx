36dc8d28d39e686d36fd4ada46dfd4463fa43f69
srprawinraja <srprawinraja@gmail.com>
2024-12-28 05:47:24 +0530

    changes

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,8 +1,8 @@
 import {Text} from 'react-native';
 import {Button} from 'react-native';
-
 import Mapbox, {Camera, LocationPuck, MapView} from '@rnmapbox/maps';
-const accessToken = 'sk.eyJ1Ijoic3JwcmF3aW5yYWphIiwiYSI6ImNtNTVxcnlocDJ2cWoyaXNmcDllaDN1Z2MifQ.n8f7sDBpFA0sDVToxzLl7w';
+import {token} from 'tokenUtils'
+const accessToken = token();
 Mapbox.setAccessToken(accessToken);
 export default function Map() {
     return <MapView style={{flex:1}} styleURL='mapbox://styles/mapbox/dark-v11'>
