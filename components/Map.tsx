b8bc9d70cf066b11c75671f33be23012fc9c74df
srprawinraja <srprawinraja@gmail.com>
2024-12-29 17:30:59 +0530

    added buttons

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,13 +1,62 @@
-import {Text} from 'react-native';
-import {Button} from 'react-native';
-import Mapbox, {Camera, LocationPuck, MapView} from '@rnmapbox/maps';
-import {token} from 'tokenUtils'
+import pin from '~/assets/pin.png';
+import React, { useState } from 'react';
+import Mapbox, {Camera, Images, LocationPuck, MapView, ShapeSource, SymbolLayer} from '@rnmapbox/maps';
+import { token } from '~/tokenUtils';
+import {featureCollection, point} from '@turf/helpers';
+import DirectionButton from '~/components/directionButton'
+import { PermissionsAndroid, ToastAndroid } from 'react-native';
+import  {Text, StyleSheet} from 'react-native';
+
 const accessToken = token()
 Mapbox.setAccessToken(accessToken);
+
 export default function Map() {
-    return <MapView style={{flex:1}} styleURL='mapbox://styles/mapbox/navigation-night-v1'>
-        <Camera followUserLocation followZoomLevel={18}/>
-        <LocationPuck/>
-        <Button title="Request Location Permission" />
-    </MapView>;
+    const [coordinates, setCoordinates] = useState<[number, number]|null>(null);
+    // Handle the map press event
+    const handleMapPress = (event:any) => {
+        setCoordinates(event.geometry.coordinates)
+        {
+        }
+    }
+    
+    return <>
+    
+    
+
+    <MapView style={styles.map} styleURL='mapbox://styles/mapbox/streets-v12' onPress={handleMapPress}>
+        {coordinates && (
+            <>
+
+        <Camera followUserLocation/>
+        <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={{isEnabled: true}}/>
+        <ShapeSource
+        id="destination"
+        shape={featureCollection([point([coordinates[0], coordinates[1]])])}>
+        <SymbolLayer id="symbol" style={{
+            iconImage:'pin',
+            iconAnchor:'bottom'}}/>
+        <Images images={{pin}}/>
+        </ShapeSource>
+        
+        </>
+      )}
+    
+        
+    </MapView>
+    {coordinates && (
+        <DirectionButton onPress={
+            ()=>{
+            }
+        }/>
+    )} 
+    
+    </>
+    
 }
+
+const styles = StyleSheet.create({
+    map: {
+        flex:1
+        
+    }
+});
---
+++ components/directionButton.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { TouchableOpacity, Image, StyleSheet, Alert } from 'react-native';
+import pin from '~/assets/pin.png';
+import { PermissionsAndroid, ToastAndroid } from 'react-native';
+
+const directionButton = ({ onPress }: { onPress: () => void }) => {
+    return (
+        <TouchableOpacity style={styles.directionButton} onPress={
+            onPress
+        } >
+        {/* Custom Icon in the Center */}
+      <Image
+        source={require('~/assets/direction.png')} // Replace with your icon path
+        style={styles.icon}
+      />
+    </TouchableOpacity>
+      );
+    }
+    
+const styles = StyleSheet.create({
+        directionButton: {
+          position: 'absolute',
+          right:40,
+          top:10,
+          alignSelf:'flex-end', // Centers the button horizontally
+
+        },
+        icon: {
+          width: 60, // Adjust icon size
+          height: 60,
+          resizeMode: 'contain', // Keep the aspect ratio of the icon
+        },
+});
+
+export default directionButton;
+
